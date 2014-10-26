14af57636ca6dc6f9bef4bc04158c5f1cfe792b9
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-10-26 21:41:21 +0000

    Implement coloured text using colored gem

--- chatbox.rb
+++ chatbox.rb
@@ -1,9 +1,18 @@
+require 'colored'
+
 def get_response(input)
   key = RESPONSES.keys.select {|k| /#{k}/ =~ input }.sample
   /#{key}/ =~ input
   response = RESPONSES[key]
   response.nil? ? 'sorry?' : response % { c1: $1, c2: $2}
 end
+
+def colorize(text, color_code)
+  "#{color_code}#{text}e[0m"
+end
+
+def red(text); colorize(text, "e[31m"); end
+def green(text); colorize(text, "e[32m"); end
 
 RESPONSES = { 'goodbye' => 'bye', 
               'sayonara' => 'sayonara', 
@@ -15,17 +24,17 @@ RESPONSES = { 'goodbye' => 'bye',
               'Have you been to (.*)\?' => '%{c1} is one of my favorite places.',
               'Can you play (.*) on a (.*)' => 'It\'s very difficult to play %{c1} with a %{c2}'}
 
- 
+ #implementation of prompts is not good...
 print 'C> '
-puts "Hello, what's your name?"
+puts "Hello, what's your name?".red
 print 'U$ '
 name = gets.chomp
 print 'C> '
-puts "Hello #{name}"
+puts "Hello #{name}".red
 print 'U$ '
 while(input = gets.chomp) do
 	exit if input == 'quit'
 	print 'C> '
-  puts get_response(input)
+  puts get_response(input).red
   print 'U$ '
 end
